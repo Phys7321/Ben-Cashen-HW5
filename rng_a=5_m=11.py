6309379e2c420fb7895784d251e88d111bb6ecc0
bencashen <43143725+bencashen@users.noreply.github.com>
2018-11-04 23:24:52 -0500

    Add files via upload

---
+++ rng_a=5_m=11.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 19:50:33 2018
+
+@author: Ben
+"""
+
+import numpy as np
+from matplotlib import pyplot
+from rng_functions import rng
+
+ngroups = 16
+
+I = np.zeros(ngroups)
+N = np.zeros(ngroups)
+Dev = np.zeros(ngroups)
+
+n0 = 100
+y = [1]
+for i in range(ngroups):
+
+    N[i] = n0
+    x = rng(y[-1],n0)
+    y = rng(x[-1],n0)
+    I[i] = 0.
+    Nin = 0
+    for j in range(n0):
+        if(y[j] < np.sqrt(1-x[j]**2)):
+            Nin += 1
+            
+    I[i] = 4.*float(Nin)/float(n0)
+    Dev[i] = abs(I[i]-np.pi)
+    print (n0,Nin,I[i],Dev[i])
+    n0 *= 2
+    
+            
+pyplot.plot(N,Dev,ls='-',c='red',lw=3,label='Deviation');
+pyplot.plot(N,1/np.sqrt(N),ls='-',c='blue',lw=3, label='1/sqrt(N)');
+pyplot.xscale('log')
+pyplot.yscale('log')
+pyplot.legend()
+pyplot.title('RNG w/ a = 5, M = 11')
---
+++ rng_functions.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  4 21:06:50 2018
+
+@author: Ben
+"""
+import numpy as np
+
+def rng(r0,N):
+    a = 5 
+    M = 11
+    r = np.zeros(N+1)
+    R = np.zeros(N+1)
+    r[0] = r0
+    for i in range(1,N+1):
+        r[i] = (a*r[i-1])%M
+        R[i] = float(r[i])/float(M)
+    return R
+
+def rng_ibm(r0,N): 
+    a = 65539 
+    M =  2**31
+    r = np.zeros(N+1)
+    R = np.zeros(N+1)
+    r[0] = r0
+    for i in range(1,N+1):
+        r[i] = (a*r[i-1])%M
+        R[i] = float(r[i])/float (M)
+    return R
